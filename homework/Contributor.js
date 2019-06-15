791043fd86e11c7280e433eda5f4c76bd9fe59f3
FaziletKosure <47929186+FaziletKosure@users.noreply.github.com>
2019-06-15 10:50:32 +0200

    Update Contributor.js

--- homework/Contributor.js
+++ homework/Contributor.js
@@ -4,16 +4,36 @@
 
 // eslint-disable-next-line no-unused-vars
 class Contributor {
-  constructor(contributor) {
-    this.contributor = contributor;
-  }
+    constructor(contributor) {
+        this.contributor = contributor;
+    }
 
-  /**
-   * Render the contributor info to the DOM.
-   * @param {HTMLElement} container The container element in which to render the contributor.
-   */
-  render(container) {
-    // TODO: replace the next line with your code.
-    Util.createAndAppend('pre', container, JSON.stringify(this.contributor, null, 2));
-  }
+
+    render(ul) {
+
+        // TODO: replace the next line with your code.
+        //Util.createAndAppend('pre', container, JSON.stringify(this.contributor, null, 2));
+        const a = Util.createAndAppend('a', ul, {
+            href: this.contributor.html_url,
+            target: '_blank',
+        })
+        const li = Util.createAndAppend('li', a, {
+            class: 'contributor_item',
+        });
+        Util.createAndAppend('img', li, {
+            src: this.contributor.avatar_url,
+            height: '50px',
+            class: 'contributor_avatar',
+        });
+        const contDataDiv = Util.createAndAppend('div', li, {
+            class: 'contributor_data',
+        });
+        Util.createAndAppend('div', contDataDiv, {
+            text: this.contributor.login,
+        });
+        Util.createAndAppend('div', contDataDiv, {
+            text: `${this.contributor.contributions}`,
+            class: 'contribution_badge',
+        });
+    }
 }
