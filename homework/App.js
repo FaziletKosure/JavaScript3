4324c5a89ba30b6cd81865b2e09b174c83625a5a
FaziletKosure <47929186+FaziletKosure@users.noreply.github.com>
2019-06-15 10:49:03 +0200

    Update App.js

--- homework/App.js
+++ homework/App.js
@@ -7,57 +7,77 @@ class App {
     this.initialize(url);
   }
 
-  /**
-   * Initialization
-   * @param {string} url The GitHub URL for obtaining the organization's repositories.
-   */
   async initialize(url) {
-    // Add code here to initialize your app
-    // 1. Create the fixed HTML elements of your page
-    // 2. Make an initial XMLHttpRequest using Util.fetchJSON() to populate your <select> element
-
     const root = document.getElementById('root');
+    const header = Util.createAndAppend('header', root, {
+      class: 'header',
+    });
+    Util.createAndAppend('p', header, {
+      text: 'HYF Repositories',
+    });
+    const selectMenu = Util.createAndAppend('select', header, {
+      class: 'repo_selector',
+    });
+    const containerDiv = Util.createAndAppend('div', root, {
+      id: 'container',
+    });
+    const repoDiv = Util.createAndAppend('div', containerDiv, {
+      class: 'left-div',
+    });
+    const table = Util.createAndAppend('table', repoDiv);
 
-    Util.createAndAppend('h1', root, { text: 'It works!' }); // TODO: replace with your own code
+    const contributorsDiv = Util.createAndAppend('div', containerDiv, {
+      class: 'right-div',
+    });
+    Util.createAndAppend('p', contributorsDiv, {
+      text: 'Contributors',
+      class: 'contributor_header',
+    });
+    const ul = Util.createAndAppend('ul', contributorsDiv, {
+      id: 'contributor_list',
+    });
+
 
     try {
       const repos = await Util.fetchJSON(url);
+      console.log(repos);
+      selectMenu.innerHTML = repos.sort((a, b) => a.name.localeCompare(b.name));
       this.repos = repos.map(repo => new Repository(repo));
       // TODO: add your own code here
+      repos.forEach(elem => {
+        Util.createAndAppend('option', selectMenu, {
+          value: repos.indexOf(elem),
+          text: elem.name
+        });
+      });
+      this.repos[0].render(table);
+      selectMenu.addEventListener('change', event => {
+        const index = event.target.value;
+        App.clearContainer(ul);
+        this.fetchContributorsAndRender(index);
+        App.clearContainer(table);
+        this.repos[index].render(table);
+
+      });
     } catch (error) {
       this.renderError(error);
     }
   }
 
-  /**
-   * Removes all child elements from a container element
-   * @param {*} container Container element to clear
-   */
   static clearContainer(container) {
     while (container.firstChild) {
       container.removeChild(container.firstChild);
     }
   }
 
-  /**
-   * Fetch contributor information for the selected repository and render the
-   * repo and its contributors as HTML elements in the DOM.
-   * @param {number} index The array index of the repository.
-   */
   async fetchContributorsAndRender(index) {
     try {
       const repo = this.repos[index];
+      console.log(repo);
       const contributors = await repo.fetchContributors();
+      console.log(contributors);
 
-      const container = document.getElementById('container');
-      App.clearContainer(container);
-
-      const leftDiv = Util.createAndAppend('div', container);
-      const rightDiv = Util.createAndAppend('div', container);
-
-      const contributorList = Util.createAndAppend('ul', rightDiv);
-
-      repo.render(leftDiv);
+      const contributorList = document.getElementById('contributor_list');
 
       contributors
         .map(contributor => new Contributor(contributor))
@@ -67,12 +87,14 @@ class App {
     }
   }
 
-  /**
-   * Render an error to the DOM.
-   * @param {Error} error An Error object describing the error.
-   */
   renderError(error) {
-    console.log(error); // TODO: replace with your own code
+    console.log(error);
+    const container = document.getElementById('root');
+    App.clearContainer(container);
+    Util.createAndAppend('div', container, {
+      text: error.message,
+      class: 'alert-error'
+    });
   }
 }
 
