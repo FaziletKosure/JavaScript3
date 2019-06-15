ecd211ee119431fffacd681686ca831b16b58c0d
FaziletKosure <47929186+FaziletKosure@users.noreply.github.com>
2019-06-15 10:54:25 +0200

    Update Repository.js

--- homework/Repository.js
+++ homework/Repository.js
@@ -4,30 +4,55 @@
 
 // eslint-disable-next-line no-unused-vars
 class Repository {
-  constructor(repository) {
-    this.repository = repository;
-  }
+    constructor(repository) {
+        this.repository = repository;
+    }
 
-  /**
-   * Render the repository info to the DOM.
-   * @param {HTMLElement} container The container element in which to render the repository.
-   */
-  render(container) {
-    // TODO: replace the next line with your code.
-    Util.createAndAppend('pre', container, JSON.stringify(this.repository, null, 2));
-  }
+    /*
+     * Render the repository info to the DOM.
+     * @param {HTMLElement} container The container element in which to render the repository.
+     */
+    render(table) {
+        // TODO: replace the next line with your code.
+        //Util.createAndAppend('pre', container, JSON.stringify(this.repository, null, 2));
+        const tr = Util.createAndAppend('tr', table);
+        Util.createAndAppend('td', tr, {
+            text: 'Repository:',
+            class: 'label',
+        });
+        const repoLink = Util.createAndAppend('td', tr);
+        Util.createAndAppend('a', repoLink, {
+            href: this.repository.svn_url,
+            target: '_blank',
+            text: this.repository.name,
+        });
+        const descriptionTr = Util.createAndAppend('tr', table);
+        Util.createAndAppend('td', descriptionTr, {
+            text: `Description: ${this.repository.description}`,
+        });
+        const forksTr = Util.createAndAppend('tr', table);
+        Util.createAndAppend('td', forksTr, {
+            text: `Forks:  ${this.repository.forks}`,
+        });
+        const updatedTr = Util.createAndAppend('tr', table);
+        Util.createAndAppend('td', updatedTr, {
+            text: `Updated:  ${this.repository.updated_at}`,
+        });
+    }
 
-  /**
-   * Returns an array of contributors as a promise
-   */
-  fetchContributors() {
-    return Util.fetchJSON(this.repository.contributors_url);
-  }
 
-  /**
-   * Returns the name of the repository
-   */
-  name() {
-    return this.repository.name;
-  }
+    /**
+     * Returns an array of contributors as a promise
+     */
+    fetchContributors() {
+        return Util.fetchJSON(this.repository.contributors_url);
+
+    }
+
+    /**
+     * Returns the name of the repository
+     */
+    name() {
+        return this.repository.name;
+    }
 }
